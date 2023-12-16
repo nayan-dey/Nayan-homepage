45d5b448bf7fb8a59542de25c83e81e754244f59
root <nayande.work@gmail.com>
2023-12-16 09:30:02 +0530

    added site name title

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -15,20 +15,20 @@ const Main = ({ children, router }) => {
     <Box as="main" pb={8}>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="description" content="Takuya's homepage" />
+        <meta name="description" content="Nayan's homepage" />
         <meta name="author" content="Nayan Dey" />
-        <meta name="author" content="craftzdog" />
+        <meta name="author" content="nayan-dey" />
         <link rel="apple-touch-icon" href="apple-touch-icon.png" />
         <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
-        <meta name="twitter:title" content="Takuya Matsuyama" />
+        <meta name="twitter:title" content="Nayan Dey" />
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content="@craftzdog" />
-        <meta name="twitter:creator" content="@craftzdog" />
-        <meta name="twitter:image" content="https://www.craftz.dog/card.png" />
-        <meta property="og:site_name" content="Takuya Matsuyama" />
-        <meta name="og:title" content="Takuya Matsuyama" />
+        <meta name="twitter:site" content="@NayanJpg" />
+        <meta name="twitter:creator" content="@NayanJpg" />
+        <meta name="twitter:image" content="nayan-dey" />
+        <meta property="og:site_name" content="Nayan Dey" />
+        <meta name="og:title" content="Nayan Dey" />
         <meta property="og:type" content="website" />
-        <meta property="og:image" content="https://www.craftz.dog/card.png" />
+        <meta property="og:image" content="nayan-dey" />
         <title>Nayan Dey - Homepage</title>
       </Head>
 
